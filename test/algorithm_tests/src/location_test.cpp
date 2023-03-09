28fe177a82601cf4d725eadbb0231ce7a8c18981
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2023-03-09 20:42:25 +0300

    Deleted test for deleted function magic_product() in 2D cases

--- test/algorithm_tests/src/location_test.cpp
+++ test/algorithm_tests/src/location_test.cpp
@@ -29,26 +29,3 @@ TEST(Location, Magic_Product_3D)
     EXPECT_EQ (Location::magic_product (pt_11, pt_31, pt_21, {802.82, -872.85, 0.0}),    Location::Loc3D::On);
     EXPECT_EQ (Location::magic_product (pt_11, pt_31, pt_21, {802.82, -872.85, -2.865}), Location::Loc3D::Above);
 }
-
-TEST(Location, Magic_Product_2D)
-{
-    Triangle T1 {Point {0.0, 0.0, 0.0}, Point {1.0, 0.0, 0.0}, Point {0.0, 1.0, 0.0}};
-    Triangle T2 {Point {0.0, 0.0, 0.0}, Point {1.0, 0.0, 0.0}, Point {2.0, 0.0, 0.0}};
-    Triangle T3 {Point {0.0, 0.0, 0.0}, Point {0.0, 0.0, 0.0}, Point {0.0, 0.0, 0.0}};
-
-    EXPECT_TRUE (Location::magic_product (T1.P_, T1.Q_, T1.R_) == Location::Loc2D::Positive &&
-                 Location::magic_product (T1.R_, T1.P_, T1.Q_) == Location::Loc2D::Positive &&
-                 Location::magic_product (T1.Q_, T1.R_, T1.P_) == Location::Loc2D::Positive);
-
-    EXPECT_TRUE (Location::magic_product (T1.Q_, T1.P_, T1.R_) == Location::Loc2D::Negative &&
-                 Location::magic_product (T1.P_, T1.R_, T1.Q_) == Location::Loc2D::Negative &&
-                 Location::magic_product (T1.R_, T1.Q_, T1.P_) == Location::Loc2D::Negative);
-
-    EXPECT_TRUE (Location::magic_product (T2.P_, T2.Q_, T2.R_) == Location::Loc2D::Neutral &&
-                 Location::magic_product (T2.R_, T2.P_, T2.Q_) == Location::Loc2D::Neutral &&
-                 Location::magic_product (T2.Q_, T2.R_, T2.P_) == Location::Loc2D::Neutral);
-
-    EXPECT_TRUE (Location::magic_product (T3.P_, T3.Q_, T3.R_) == Location::Loc2D::Neutral &&
-                 Location::magic_product (T3.R_, T3.P_, T3.Q_) == Location::Loc2D::Neutral &&
-                 Location::magic_product (T3.Q_, T3.R_, T3.P_) == Location::Loc2D::Neutral);
-}

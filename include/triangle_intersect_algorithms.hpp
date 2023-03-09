bbd208f94513ee25364a39f77565fb1c5d98d910
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2023-03-09 20:35:48 +0300

    Deleted space_transformation() and rewrote algorithm of analysis 2D cases

--- include/triangle_intersect_algorithms.hpp
+++ include/triangle_intersect_algorithms.hpp
@@ -8,133 +8,121 @@ namespace Geometry
 namespace Algorithm
 {
 
-template<std::floating_point F>
-void point_transformation (Point<F> &point, const Point<F> &origin, const Vector<F> &x_axis,
-                           const Vector<F> &y_axis)
+//  prds - Position Related to Direction on the Plane 
+template <std::floating_point F>
+Location::Loc2D define_prds (const Point<F>& def_point, const Point<F>& vec_origin, 
+                             const Point<F>& vec_end, const Vector<F>& normal)
 {
-    auto diff_x = point.x_ - origin.x_;
-    auto diff_y = point.y_ - origin.y_;
-    auto diff_z = point.z_ - origin.z_;
-
-    point.x_ = x_axis.x_ * diff_x + x_axis.y_ * diff_y + x_axis.z_ * diff_z;
-    point.y_ = y_axis.x_ * diff_x + y_axis.y_ * diff_y + y_axis.z_ * diff_z;
-    point.z_ = 0.0;
-}
-
-template<std::floating_point F>
-void space_transformation (Triangle<F> &tr_1, Triangle<F> &tr_2)
-{
-    Vector x_axis {tr_1.P_, tr_1.Q_};
-    x_axis *= (1 / x_axis.module ());
-
-    Vector normal = vector_product (Vector {tr_1.P_, tr_1.Q_}, Vector {tr_1.P_, tr_1.R_});
-    normal *= (1 / normal.module ());
-
-    Vector y_axis = vector_product (normal, x_axis);
-
-    point_transformation (tr_1.Q_, tr_1.P_, x_axis, y_axis);
-    point_transformation (tr_1.R_, tr_1.P_, x_axis, y_axis);
-
-    point_transformation (tr_2.P_, tr_1.P_, x_axis, y_axis);
-    point_transformation (tr_2.Q_, tr_1.P_, x_axis, y_axis);
-    point_transformation (tr_2.R_, tr_1.P_, x_axis, y_axis);
-    tr_1.P_ = {0.0, 0.0, 0.0}; //  this is origin
-}
-
-template<std::floating_point F>
-bool test_intersection_R1 (const Triangle<F> &tr_1, const Triangle<F> &tr_2)
-{
-    if (Location::magic_product (tr_2.R_, tr_2.P_, tr_1.Q_) == Location::Loc2D::Negative)
+    Vector<F> normal_def_point = vector_product (Vector<F> {def_point, vec_origin},
+                                                 Vector<F> {def_point, vec_end}).normalize ();
+    auto sign = static_cast<int>(scalar_product (normal, normal_def_point));
+    //  Can I remake this with casting?
+    switch (sign)
     {
-        return !(Location::magic_product (tr_2.R_, tr_2.P_, tr_1.R_) == Location::Loc2D::Negative ||
-                 Location::magic_product (tr_1.Q_, tr_1.R_, tr_2.R_) == Location::Loc2D::Negative ||
-                 Location::magic_product (tr_1.P_, tr_2.P_, tr_1.R_) == Location::Loc2D::Negative);
+        case -1:
+            return Location::Loc2D::Right;
+        case 0:
+            return Location::Loc2D::On;
+        case 1:
+            return Location::Loc2D::Left;
+        default:
+            throw std::runtime_error ("Objects are located in different planes\n");
+    }
+}
+
+//  Vector normal which defined orientation of tr_1
+template <std::floating_point F>
+bool test_intersection_R1 (const Triangle<F> &tr_1, const Triangle<F> &tr_2, const Vector<F>& normal)
+{   
+    if (define_prds (tr_2.R_, tr_2.P_, tr_1.Q_, normal) == Location::Loc2D::Right)
+    {
+        return !(define_prds (tr_2.R_, tr_2.P_, tr_1.R_, normal) == Location::Loc2D::Right ||
+                 define_prds (tr_1.Q_, tr_1.R_, tr_2.R_, normal) == Location::Loc2D::Right ||
+                 define_prds (tr_1.P_, tr_2.P_, tr_1.R_, normal) == Location::Loc2D::Right);
     }
     else 
     {
-        if (Location::magic_product (tr_2.R_, tr_1.P_, tr_1.Q_) == Location::Loc2D::Negative)
+        if (define_prds (tr_2.R_, tr_1.P_, tr_1.Q_, normal) == Location::Loc2D::Right)
             return false;
-        else if (Location::magic_product (tr_1.P_, tr_2.P_, tr_1.Q_) != Location::Loc2D::Negative)
+        else if (define_prds (tr_1.P_, tr_2.P_, tr_1.Q_, normal) != Location::Loc2D::Right)
             return true;
-        else if (Location::magic_product (tr_1.P_, tr_2.P_, tr_1.R_) == Location::Loc2D::Negative ||
-                 Location::magic_product (tr_1.Q_, tr_1.R_, tr_2.P_) == Location::Loc2D::Negative)
+        else if (define_prds (tr_1.P_, tr_2.P_, tr_1.R_, normal) == Location::Loc2D::Right ||
+                 define_prds (tr_1.Q_, tr_1.R_, tr_2.P_, normal) == Location::Loc2D::Right)
             return false;
         else
             return true;
     }
 }
 
-template<std::floating_point F>
-bool test_intersection_R2 (const Triangle<F> &tr_1, const Triangle<F> &tr_2)
+template <std::floating_point F>
+bool test_intersection_R2 (const Triangle<F>& tr_1, const Triangle<F>& tr_2, const Vector<F>& normal)
 {
-    if (Location::magic_product (tr_2.R_, tr_2.P_, tr_1.Q_) == Location::Loc2D::Negative)
+    if (define_prds (tr_2.R_, tr_2.P_, tr_1.Q_, normal) == Location::Loc2D::Right)
     {
-        if (Location::magic_product (tr_2.R_, tr_2.P_, tr_1.R_) == Location::Loc2D::Negative)
+        if (define_prds (tr_2.R_, tr_2.P_, tr_1.R_, normal) == Location::Loc2D::Right)
             return false;
-        else if (Location::magic_product (tr_1.Q_, tr_1.R_, tr_2.R_) == Location::Loc2D::Negative)
+        else if (define_prds (tr_1.Q_, tr_1.R_, tr_2.R_, normal) == Location::Loc2D::Right)
         {
-            return !(Location::magic_product (tr_1.Q_, tr_1.R_, tr_2.Q_) == Location::Loc2D::Negative ||
-                     Location::magic_product (tr_2.Q_, tr_2.R_, tr_1.R_) == Location::Loc2D::Negative);
+            return !(define_prds (tr_1.Q_, tr_1.R_, tr_2.Q_, normal) == Location::Loc2D::Right ||
+                     define_prds (tr_2.Q_, tr_2.R_, tr_1.R_, normal) == Location::Loc2D::Right);
         }
         else
-            return !(Location::magic_product (tr_1.R_, tr_1.P_, tr_2.P_) == Location::Loc2D::Negative);
+            return !(define_prds (tr_1.R_, tr_1.P_, tr_2.P_, normal) == Location::Loc2D::Right);
     }
     else
     {
-        if (Location::magic_product (tr_2.Q_, tr_2.R_, tr_1.Q_) == Location::Loc2D::Negative)
+        if (define_prds (tr_2.Q_, tr_2.R_, tr_1.Q_, normal) == Location::Loc2D::Right)
         {
-            return !(Location::magic_product (tr_1.P_, tr_2.Q_, tr_1.Q_) == Location::Loc2D::Positive ||
-                     Location::magic_product (tr_2.Q_, tr_2.R_, tr_1.R_) == Location::Loc2D::Negative ||
-                     Location::magic_product (tr_1.Q_, tr_1.R_, tr_2.Q_) == Location::Loc2D::Negative);
+            return !(define_prds (tr_1.P_, tr_2.Q_, tr_1.Q_, normal) == Location::Loc2D::Left ||
+                     define_prds (tr_2.Q_, tr_2.R_, tr_1.R_, normal) == Location::Loc2D::Right ||
+                     define_prds (tr_1.Q_, tr_1.R_, tr_2.Q_, normal) == Location::Loc2D::Right);
         }
         else
         {
-            if (Location::magic_product (tr_1.P_, tr_2.P_, tr_1.Q_) == Location::Loc2D::Negative)
+            if (define_prds (tr_1.P_, tr_2.P_, tr_1.Q_, normal) == Location::Loc2D::Right)
             {
-                return !(Location::magic_product (tr_1.P_, tr_2.P_, tr_1.R_) == Location::Loc2D::Negative ||
-                         Location::magic_product (tr_2.R_, tr_2.P_, tr_1.R_) == Location::Loc2D::Negative);
+                return !(define_prds (tr_1.P_, tr_2.P_, tr_1.R_, normal) == Location::Loc2D::Right ||
+                         define_prds (tr_2.R_, tr_2.P_, tr_1.R_, normal) == Location::Loc2D::Right);
             }
             else 
-                return !(Location::magic_product (tr_1.P_, tr_2.Q_, tr_1.Q_) == Location::Loc2D::Positive);
+                return !(define_prds (tr_1.P_, tr_2.Q_, tr_1.Q_, normal) == Location::Loc2D::Left);
         }
     }
 }
 
-template<std::floating_point F>
-bool intersection_in_2D (const Triangle<F> &tr_1_, const Triangle<F> &tr_2_)
+template <std::floating_point F>
+bool intersection_in_2D (const Triangle<F>& tr_1_, const Triangle<F>& tr_2_)
 {
-    Triangle tr_1 = tr_1_;
-    Triangle tr_2 = tr_2_;
-    space_transformation (tr_1, tr_2);
+    Triangle<F> tr_1 = tr_1_, tr_2 = tr_2_;
+    //  Return a normal vector by the 1st triangle
+    Vector<F> normal = vector_product (Vector<F> {tr_1.P_, tr_1.Q_}, 
+                                       Vector<F> {tr_1.P_, tr_1.R_}).normalize();
+    std::cout << "normal: (" << normal.x_ << " " << normal.y_ << " " << normal.z_ << ")" << std::endl;
 
-    //  if swap tr_2.Q_ and tr_2.R_
-    if (Location::magic_product (tr_2.P_, tr_2.Q_, tr_2.R_) == Location::Loc2D::Negative)
+    //  Swap tr_2.Q() and tr_2.R() if P2, Q2, R2 are clockwise
+    if (define_prds (tr_2.P_, tr_2.Q_, tr_2.R_, normal) == Location::Loc2D::Right)
         tr_2.swap_QR ();
 
-    auto P2_loc = Location::magic_product (tr_1.P_, tr_1.Q_, tr_2.P_) *
-                  Location::magic_product (tr_1.Q_, tr_1.R_, tr_2.P_) *
-                  Location::magic_product (tr_1.R_, tr_1.P_, tr_2.P_);
+    auto P1_P2_Q2 = define_prds (tr_1.P_, tr_2.P_, tr_2.Q_, normal);
+    auto P1_Q2_R2 = define_prds (tr_1.P_, tr_2.Q_, tr_2.R_, normal);
+    auto P1_R2_P2 = define_prds (tr_1.P_, tr_2.R_, tr_2.P_, normal);
 
-    auto P1_loc = Location::Loc2D::Positive;
-
-    auto P1_P2_Q2 = Location::Loc2D::Is_vertice;
-    auto P1_Q2_R2 = Location::Loc2D::Is_vertice;
-    auto P1_R2_P2 = Location::Loc2D::Is_vertice;
-    auto sum_locs = Location::Loc2D::Is_vertice;
-
-    //  interior of tr_2
-    if (P1_P2_Q2 == Location::Loc2D::Positive &&
-        P1_Q2_R2 == Location::Loc2D::Positive && 
-        P1_R2_P2 == Location::Loc2D::Positive)
+    //  Define position of P1
+    if (P1_P2_Q2 == Location::Loc2D::Left && P1_Q2_R2 == Location::Loc2D::Left && P1_R2_P2 == Location::Loc2D::Left)
         return true;
-    else if (P1_loc == Location::Loc2D::Neutral &&
-            (sum_locs == Location::Loc2D::Is_vertice || sum_locs == Location::Loc2D::On_side))
+    else if (tr_1.P_ == tr_2.P_ || tr_1.P_ == tr_2.Q_ || tr_1.P_ == tr_2.R_)
         return true;
-    else if (P1_loc == Location::Loc2D::Negative ||
-            ((P1_loc == Location::Loc2D::Neutral) && (sum_locs == Location::Loc2D::Neutral)))
-        return test_intersection_R1 (tr_1, tr_2);
+    else if ((P1_P2_Q2 * P1_Q2_R2 * P1_R2_P2 == Location::Loc2D::On) &&
+             (P1_P2_Q2 != Location::Loc2D::Right && P1_Q2_R2 != Location::Loc2D::Right ||
+              P1_Q2_R2 != Location::Loc2D::Right && P1_R2_P2 != Location::Loc2D::Right ||
+              P1_R2_P2 != Location::Loc2D::Right && P1_P2_Q2 != Location::Loc2D::Right))
+        return true;
+    else if (P1_P2_Q2 == Location::Loc2D::Left && P1_Q2_R2 == Location::Loc2D::Left ||
+             P1_Q2_R2 == Location::Loc2D::Left && P1_R2_P2 == Location::Loc2D::Left ||
+             P1_R2_P2 == Location::Loc2D::Left && P1_P2_Q2 == Location::Loc2D::Left) 
+        return test_intersection_R1 (tr_1, tr_2, normal);
     else
-        return test_intersection_R2 (tr_1, tr_2);
+        return test_intersection_R2 (tr_1, tr_2, normal);
 }
 
 template<std::floating_point F>
